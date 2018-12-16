a78f1fbd1abcddf7c408e9b5ec5dddffe843bc74
Zach Owen <zach.owen50@gmail.com>
2018-12-15 18:03:03 -0800

    Process at rank 0 reads from test file. Changed test1 to txt file.

--- dense_transpose.py
+++ dense_transpose.py
@@ -1,11 +1,29 @@
-from numpy import transpose
+import os
+import sys
+#from numpy import transpose
 from mpi4py import MPI
 
-def main():
 
+def main(argc, argv):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    print("test")
+    if rank == 0:
+	matrix_file = open(argv[1], "rt")
+	
+	dimension = matrix_file.readline().strip().split()
+	row_total = dimension[0]
+	col_total = dimension[1]
+	
+	print("Matrix from " + argv[1] + ": ")
+	for row in matrix_file:
+	    print(row.strip())
+	
+	matrix_file.close()
+    else:
+	print("I am process " + str(rank))
 
+
+if __name__ == "__main__":
+    main(len(sys.argv), sys.argv)
