828b9f3cc1309147cccd782308d4668ed976aa9f
Zach Owen <zach.owen50@gmail.com>
2018-12-17 14:02:39 -0800

    Added gather to sort the messages.

--- main.py
+++ main.py
@@ -17,23 +17,27 @@ def main(argc, argv):
         dimension = matrix_file.readline().strip().split()
         
         for line in matrix_file:
-            lines = lines + 1
             this_line = line.strip().split()
             data = data + this_line
         
         comm.Barrier()
-    
+
     else:
         comm.Barrier()	
     
     i = 0
     j = 0
     while i < 4:
-        value.append(comm.scatter(data[j:j+size], root=0))
+        value.append(comm.scatter(data[j:j+size], root=0)) #
         j = j + size
         i = i + 1
     
     print("I am process " + str(rank) + " and I have " + str(value))
+
+    new_data = comm.gather(value, root=0)
+    if rank == 0:
+	for i in range(size):
+	    print(new_data[i])
 
 if __name__ == "__main__":
     main(len(sys.argv), sys.argv)
