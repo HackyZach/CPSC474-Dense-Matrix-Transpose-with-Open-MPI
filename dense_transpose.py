830f7fa5610919587d2d245c5c3a0a6ef31191bd
Zach Owen <zach.owen50@gmail.com>
2018-12-16 16:35:32 -0800

    Changed data to an array to loop through.

--- dense_transpose.py
+++ dense_transpose.py
@@ -1,6 +1,5 @@
 import os
 import sys
-#import numpy as np
 from mpi4py import MPI
 
 
@@ -8,37 +7,38 @@ def main(argc, argv):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+    data = []
+    row_total = 0
 
     if rank == 0:
-        data = [(i+1)**2 for i in range(size)]
+        #data = [(i+1)**2 for i in range(size)] #Works fine without, not sure of purpose.
         matrix_file = open(argv[1], "rt")
 	
 	dimension = matrix_file.readline().strip().split()
-        #row_total = dimension[0]
 	#col_total = dimension[1]
         
-        data = matrix_file.readline().strip().split()
-        data1 = matrix_file.readline().strip().split()
-        data2 = matrix_file.readline().strip().split()
-        data3 = matrix_file.readline().strip().split()
+	row_total = 0
+	for line in matrix_file:
+	    line = line.strip().split()
+	    data.append(line)
+	    row_total += 1
+
 	#for row in matrix_file:
         #    row = row.strip().split()
         #    row = comm.Scatter(row[0:10], local_a[:], root=0)
         
         comm.Barrier() 
     else:
-        data = None
-        data1 = None
-        data2 = None
-        data3 = None
+	for i in range(4):
+	    data.append(None)
         comm.Barrier()	
 
-    data = comm.scatter(data, root=0)
-    data1 = comm.scatter(data1, root=0)
-    data2 = comm.scatter(data2, root=0)
-    data3 = comm.scatter(data3, root=0)
+    data[0] = comm.scatter(data[0], root=0)
+    data[1] = comm.scatter(data[1], root=0)
+    data[2] = comm.scatter(data[2], root=0)
+    data[3] = comm.scatter(data[3], root=0)
 
-    wow = [data, data1, data2, data3]
+    wow = [data[0], data[1], data[2], data[3]]
 
     print("I am process " + str(rank) + " and I have " + str(wow))
 
